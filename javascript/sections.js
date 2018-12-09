9282bd4f64e6dc307185d3272128f83b0b277f9e
nguyen41v <nguyen41v@gmail.com>
2018-12-08 18:06:01 -0800

    Moved javascript code into sections.js

--- javascript/sections.js
+++ javascript/sections.js
@@ -1,3 +1,16 @@
-$(document).ready(function() {
-  $('.accordion-item-1 .accordion-content').show();
-});
+(function(){
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = 0;
+        } 
+      });
+    }
+})();
